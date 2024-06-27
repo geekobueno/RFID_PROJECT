f710575996e23c88cc4834f2bf4c314377336c90
GeekoBueno <geeklove2222@gmail.com>
2024-06-27 13:51:25 +0000

    update server

--- backend/index.js
+++ backend/index.js
@@ -244,6 +244,7 @@ app.delete('/api/rfidtags/:cardUID', async (req, res) => {
 });
 
 // Start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
